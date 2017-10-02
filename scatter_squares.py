24b86366332618bebd60d80780283c39548604b2
link375 <https://github.com/link375>
2017-10-01 22:40:38 -0600

    add secondary plot

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,4 +1,16 @@
 import matplotlib.pyplot as plt
 
-plt.scatter(2, 4)
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
+
+plt.scatter(x_values, y_values, s=200)
+
+# set chart title and label axes
+plt.title("Square Number", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+
+# set size of tick labels
+plt.tick_params(axis='both', which='major', labelsize=14)
+
 plt.show()
